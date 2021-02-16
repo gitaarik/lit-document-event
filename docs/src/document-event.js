6a0884f81f8f84000bfe9e8ec1da69a212156c1e
Rik <gitaarik@posteo.net>
2021-02-15 20:50:02 -0500

    changed symlink to normal link

--- docs/src/document-event.js
+++ docs/src/document-event.js
@@ -1,1 +1,61 @@
-../../document-event.js
+import { LitElement, html } from 'lit-element';
+
+
+class DocumentEvent extends LitElement {
+
+  static get properties() {
+    return {
+      eventname: {type: String}
+    };
+  }
+
+  render() {
+
+    /*
+     * NOTE: The <slot> element needs a container (the `slot-container`), other
+     * than a custom element. Otherwise it won't render in webcomponent
+     * polyfilled browsers (like IE).
+     */
+
+    return html`
+      <div id="slot-container">
+        <slot></slot>
+      </div>
+    `;
+
+  }
+
+  connectedCallback() {
+    super.connectedCallback();
+    this.addDocumentEventListener();
+  }
+
+  disconnectedCallback() {
+    super.disconnectedCallback();
+    this.removeDocumentEventListener();
+  }
+
+  addDocumentEventListener() {
+
+    this.eventListener = event => {
+      this.dispatchEvent(
+        new CustomEvent('event', {detail: event})
+      );
+    };
+
+    // Use `setTimeout` to add the listener at the next event cycle. This
+    // prevents the event from directly firing when the component was created
+    // by an event with the same name.
+    setTimeout(() => {
+      document.addEventListener(this.eventname, this.eventListener);
+    });
+
+  }
+
+  removeDocumentEventListener() {
+    document.removeEventListener(this.eventname, this.eventListener);
+  }
+
+}
+
+customElements.define('document-event', DocumentEvent);
